a53be7507789056c4e820130e92d87413677a8d9
MamtaSaini1997 <mmtsaini1997@gmail.com>
2025-05-22 17:19:16 +0530

    forth program

---
+++ chapter_1/forth_program.py
@@ -0,0 +1,5 @@
+# wap to input two numbers, a and b
+#print true if a is greater than or equal to b. if not print false.
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+print(a >= b)
