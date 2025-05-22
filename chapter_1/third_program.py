c759ea514f29165dc119ecf105bb11a4b34b2697
MamtaSaini1997 <mmtsaini1997@gmail.com>
2025-05-22 17:01:21 +0530

    third program

---
+++ chapter_1/third_program.py
@@ -0,0 +1,5 @@
+# wap to input two floating numbers and print their average
+a = float(input("enter first number: "))
+b = float(input("enter second number: "))
+avg = (a + b)/2
+print("Averge is: ", avg)
