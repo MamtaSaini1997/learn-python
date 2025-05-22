5e68452130a0a911811200ea08b51f3fdcf8bedf
MamtaSaini1997 <mmtsaini1997@gmail.com>
2025-05-22 22:52:12 +0530

    chapter 2 done

---
+++ chapter_2/fifth_program.py
@@ -0,0 +1,6 @@
+#wap to check if a number is a multiple of 7 or not.
+num = int(input("enter number: "))
+if(num % 7 == 0):
+    print("number is multiple of 7")
+else:
+    print("number is not a multiple of 7")
--- chapter_2/forth_program.py
+++ chapter_2/forth_program.py
@@ -1,1 +1,10 @@
-#
+#wap to find the greatest of 3 numbers entered by the user.
+a = int(input("enter ist number: "))
+b = int(input("enter second number: "))
+c = int(input("enter third number: "))
+if(a >= b and a >= c):
+    print("a is greatest")
+elif(b >= c):
+    print("b is greatest")
+else:
+    print("c is greatest")
